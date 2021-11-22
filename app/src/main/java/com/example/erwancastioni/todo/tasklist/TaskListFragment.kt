abfeddf5db85b0d4a27ca569daf7235aca0a4cf2
Malan-Tai <malantai99@gmail.com>
2021-11-22 15:32:11 +0100

    step 8 : add task btn

--- app/src/main/java/com/example/erwancastioni/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/erwancastioni/todo/tasklist/TaskListFragment.kt
@@ -8,10 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.erwancastioni.todo.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class TaskListFragment: Fragment() {
 
-    private val taskList = listOf(
+    private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
@@ -29,5 +31,12 @@ class TaskListFragment: Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+
+        val addTaskBtn = view.findViewById<FloatingActionButton>(R.id.add_task_btn)
+        addTaskBtn.setOnClickListener {
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(newTask)
+            recyclerView.adapter?.notifyItemInserted(taskList.size - 1)
+        }
     }
 }
